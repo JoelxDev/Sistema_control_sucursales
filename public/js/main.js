566d0d7ebdc870a006e0d786043e77522f8b7e5a
joeladriano123 <joeladriano.jl@gmail.com>
2025-04-17 07:38:43 -0500

    commit para los archivos de la carpeta css, img, js e index dentro de /public

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+// Sucursales
+function A_sucursal(){
+    window.location.href='../sucursales/anadir_sucursal.html';
+    // El 'href' no es una funcion, es una propiedad de objeto 'location' y este de la funcion 'window'
+}
+// Usuarios
+function C_usuarios() {
+    window.location.href='../usuarios/btn_crear_us.html';
+}
+
+// Reporte de Ventas
+function H_ventas() {
+    window.location.href='../reporte_ventas/btn_historialVe.html';
+}
+
+// Inventarios, productos
+function btn_VProductos() {
+    window.location.href='../inventario/btn_VProductos.html';
+}
+
+// Inventario, añadir productos
+function btn_AProducto(){
+    window.location.href='../inventario/btn_AProducto.html';
+} 
+// Boton para ver el historial de pedidos 
+function btn_HPedidos(){
+    window.location.href='../pedidos/btn_HPedidos.html';
+}
+
+// Ventas registradas del usuario estandar
+
+function btn_ventasRegistradasUE(){
+    window.location.href='btn_ventasRegistradasUE.html';
+}
+
+function btn_actualizarInventarioUE(){
+    window.location.href='btn_actualizarInventario.html';
+}
